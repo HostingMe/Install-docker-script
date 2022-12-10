71c754a8741ebad430d12c3fed3b5749b445ec94
Steve <110924473+stevennoad@users.noreply.github.com>
2022-12-10 20:40:48 +0000

    Initial upload
    Initial upload of script that installs Docker on Ubuntu.

---
+++ docker-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#docker_installation
+
+if [ ! -x /var/lib/docker ]; then
+      
+echo "Installing docker..."
+
+#install docker dependencies 
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+
+#add docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#set up the stable repository.
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#update the packages
+apt update -y
+
+#install Docker from their repo instead
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#adds your username to the docker group
+usermod -aG docker ${USER}
+       
+echo "docker successfully installed."
+
+else
+
+echo "Docker already installed, nothing to do."
+
+fi
