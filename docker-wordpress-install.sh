f76b7f0534e105dbeb93169b2e9b140817f4ef91
Steve <110924473+stevennoad@users.noreply.github.com>
2022-12-10 21:52:53 +0000

    [Bug] fixed sudo issue
    Fixed an issue where docker run would not run for non-root users. Added sudo to command

--- docker-wordpress-install.sh
+++ docker-wordpress-install.sh
@@ -36,9 +36,9 @@ echo "docker already installed, installing wordpress and mariaDB..."
 mkdir ~/wordpress && cd ~/wordpress
 
 #pull the mariaDB image from docker
-docker run -e MYSQL_ROOT_PASSWORD=enter-your-password -e MYSQL_DATABASE=wordpress --name wordpressdb -v "$PWD/database":/var/lib/mysql -d mariadb:latest
+sudo docker run -e MYSQL_ROOT_PASSWORD=enter-your-password -e MYSQL_DATABASE=wordpress --name wordpressdb -v "$PWD/database":/var/lib/mysql -d mariadb:latest
 
 #pull the wordpress image from docker
-docker run -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=enter-your-password --name wordpress --link wordpressdb:mysql -p 80:80 -v "$PWD/html":/var/www/html -d wordpress
+sudo docker run -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=enter-your-password --name wordpress --link wordpressdb:mysql -p 80:80 -v "$PWD/html":/var/www/html -d wordpress
 
 fi
