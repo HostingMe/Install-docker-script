14cbf2f868e1b724a09198ff99b01e122ad2ee80
Steve <110924473+stevennoad@users.noreply.github.com>
2022-12-10 21:36:13 +0000

    Create docker-wordpress-install.sh

---
+++ docker-wordpress-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#docker installation      
+echo "Installing docker..."
+
+#install docker dependencies 
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+
+#add docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#set up the stable repository.
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#update the packages
+sudo apt update -y
+
+#install Docker from their repo instead
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+#adds your username to the docker group
+sudo usermod -aG docker ${USER}
+       
+echo "docker successfully installed."
+
+#create local directories for mariaDB and Wordpress
+mkdir ~/wordpress && cd ~/wordpress
+
+#pull the mariaDB image from docker
+docker run -e MYSQL_ROOT_PASSWORD=enter-your-password -e MYSQL_DATABASE=wordpress --name wordpressdb -v "$PWD/database":/var/lib/mysql -d mariadb:latest
+
+#pull the wordpress image from docker
+docker run -e WORDPRESS_DB_USER=root -e WORDPRESS_DB_PASSWORD=enter-your-password --name wordpress --link wordpressdb:mysql -p 80:80 -v "$PWD/html":/var/www/html -d wordpress
+
+fi
